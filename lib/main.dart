3177aeca0979c7c029acd24b3f5695ceb64f03b1
Your Name <you@example.com>
2026-07-07 23:52:02 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+void main(){
+
+}
+class TounsApp extends StatelessWidget {
+  const TounsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
